090614ebf7367b25a2fc415d76a8a1e0a56b7bf3
Vincent <jan-vincent.wyszynski@charite.de>
2017-11-29 18:58:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const TelegramBot = require('node-telegram-bot-api');
+const options = {
+  webHook: {
+    port: process.env.PORT
+  }
+};
+
+const url = process.env.APP_URL;
+const bot = new TelegramBot(TOKEN, options);
+
+
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
+});
